da096a95b8ecddbcecdb9f4fd6fa1df5f628d0aa
Volodymyr Myronenko <v.mironenko01@gmail.com>
2021-06-08 17:07:19 +0300

    Updates

---
+++ precache-manifest.4566645c77aa93811bc592c4de3b62d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafcb102d1221dc23b112df749a184b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5620cb1e3dcf4e295ee7",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "d7e9e5979610d69286d9",
+    "url": "./static/css/main.d3923686.chunk.css"
+  },
+  {
+    "revision": "5620cb1e3dcf4e295ee7",
+    "url": "./static/js/2.6d4a844e.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.6d4a844e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e9e5979610d69286d9",
+    "url": "./static/js/main.21f224b6.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
